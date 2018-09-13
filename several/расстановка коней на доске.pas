680d6e0062f8f08da2f456d60728c653a778b5bb
Alexey-coder <alexey.user.1988@gmail.com>
2018-09-13 15:20:02 +0300

    fst

--- several/расстановка коней на доске.pas
+++ several/расстановка коней на доске.pas
@@ -1,18 +1,25 @@
-﻿    var matr:array [,] of Boolean;count:int64:=0;
+﻿    var x:array [,] of Boolean;count:int64:=0;
     procedure Show;
     begin
       WriteLn(count);
-      WriteLn(matr.Rows.Select(r->r.Select(x->x?' K':' .').JoinIntoString).JoinIntoString(NewLine));
+      WriteLn(x.Rows.Select(r->r.Select(x->x?' K':' .').JoinIntoString).JoinIntoString(NewLine));
     end;
     procedure Конь(Ещё,Row,Col:Integer);
     begin
-      if Ещё=0 then begin count+=1;if count mod 100000000=0 then Show;end
-      else while Row<8 do begin while Col<8 do begin
-        matr[Row,Col]:=True;Конь(Ещё-1,Row,Col+1);matr[Row,Col]:=False;Col+=1;end;
-        Col:=0;Row+=1;end;
+      if Ещё=0 then begin 
+      count+=1;
+      if count mod 100000000=0 then Show;
+      end
+      else while Row<8 do begin 
+      while Col<8 do begin
+        x[Row,Col]:=True;Конь(Ещё-1,Row,Col+1);
+        x[Row,Col]:=False;Col+=1;
+        end;
+        Col:=0;Row+=1;
+        end;
     end;
     begin
-      SetLength(matr,8,8);
+      SetLength(x,8,8);
       Конь(8,0,0);
       WriteLn(count);
     end.
