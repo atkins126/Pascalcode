000c584639095b2114520776464590286dd44131
Alexey-coder <Alexey.user.1988@gmail.com>
2018-11-19 20:05:24 +0300

    litle more coede

---
+++ several/sem1.pas
@@ -0,0 +1,14 @@
+﻿Var
+x, sum, s: real;
+i, n: integer;
+begin
+sum := 0;
+read(n, x);
+s := 1;
+for i := 1 to n do
+begin
+s := s * sin(x);
+sum := sum + s;
+end;
+writeln(sum);
+end. 
---
+++ several/tab3.pas
@@ -0,0 +1,26 @@
+﻿function F(x:real):real;
+begin
+F:=tan(x);
+end;
+var a,b,h,x:real;
+    k:integer;
+begin
+h:=0.1;
+repeat
+writeln('Введите интервал [a,b]');
+readln(a,b);
+until a<b-h;
+x:=a;
+k:=0;
+while x<=b+h/2 do
+ begin
+  writeln('x=',x:4:1,'  F(x)=',F(x):6:3);
+  inc(k);
+  if k mod 23=0 then
+   begin
+    write('Press Enter');
+    readln
+   end;
+  x:=x+h
+ end;
+end.
