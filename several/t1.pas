4227261a309f70fe720e62e41b6288bd88edecc4
Alexey-coder <Alexey.user.1988@gmail.com>
2018-11-29 15:19:25 +0300

    litle more coede

--- several/t1.pas
+++ several/t1.pas
@@ -3,8 +3,25 @@
 Изменить их значения, переместив содержимое
  A в B, B — в C, C — в A, и вывести новые значения переменных A, B, C. 
 }
-var a,b,c:integer;
+var
+  A, B, C, v: real;
+
 begin
-Read(a,b,c);
-Write('старые хначения',' ',a,' ',b,' ',c );
+  write('A = ');
+  readln(A);
+  write('B = ');
+  readln(B);
+  write('C = ');
+  readln(C);
+  { A и C меняем местами: }
+  v := A;
+  A := C;
+  C := v;
+  { B и C меняем местами: }
+  v := B;
+  B := C;
+  C := v;
+  writeln('Результат:');
+  writeln(' A = ', A, ', B = ', B, ', C = ', C);
+  readln
 end.
