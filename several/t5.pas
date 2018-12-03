c2d54b40aaaec380cdcb334a6f71ed164107116b
Alexey-coder <Alexey.user.1988@gmail.com>
2018-12-03 10:19:51 +0300

    litle more coede

---
+++ several/t5.pas
@@ -0,0 +1,21 @@
+﻿{Дан целочисленный массив размера N. 
+Вывести все содержащиеся в данном массиве четные числа в порядке возрастания их индексов, а также их количество.}
+Program Task;
+Const N = 5;
+Var a: Array [1..N] of Integer;
+i, k: Integer;
+Begin
+WriteLn('Введите ', N, ' целых чисел через пробел');
+For i := 1 To N Do
+Read(a[i]);
+For i := 1 To N Do
+If a[i] Mod 2 = 0 Then
+Begin
+Write(a[i], ' ');
+k := k + 1;
+End;
+WriteLn;
+WriteLn('Всего: ', k);
+ReadLn;
+End. 
+
