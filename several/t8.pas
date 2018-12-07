834ce078fdb960e4353de4d3b044552781c4e645
Alexey-coder <Alexey.user.1988@gmail.com>
2018-12-07 18:07:12 +0300

    proj

---
+++ several/t8.pas
@@ -0,0 +1,27 @@
+﻿uses crt;
+function F(x:real):real;
+begin
+F:=2*sin(x)+1
+end;
+var a,b,h,x:real;
+    k:integer;
+begin
+h:=0.1;
+repeat
+writeln('Введите интервал [a,b]');
+readln(a,b);
+until a<b-h;
+x:=a;
+k:=0;
+while x<=b+h/2 do
+ begin
+  writeln('x=',x:4:1,'  F(x)=',F(x):6:3);
+  inc(k);
+  if k mod 23=0 then
+   begin
+    write('Press Enter');
+    readln
+   end;
+  x:=x+h
+ end;
+end.
