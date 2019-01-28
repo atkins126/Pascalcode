b5fdbb3cf410135639a2db7d29cd7699430b32ee
Alexey-coder <Alexey.user.1988@gmail.com>
2019-01-28 23:04:09 +0300

    1

---
+++ several/forums/filemax.pas
@@ -0,0 +1,42 @@
+﻿var fp:text;
+var s1,s:string;
+var i,n:integer;
+procedure sozd(var fp:text);
+begin
+Assign(fp,'mfile.TXT');
+Rewrite (fp);
+close(fp);
+Assign(fp,'mfile.txt');
+Rewrite(fp);
+Writeln(fp,'труд');
+Writeln(fp,'мир ');
+Writeln(fp,'радола');
+Close(fp);
+end;
+procedure chten(var fp:text);
+var max:integer;
+begin
+max:=0;
+i:=0;
+n:=0;
+Assign(fp,'mfile.txt');
+Reset(fp);
+while not(Eof(fp)) do
+begin
+Readln(fp,s);
+inc(i);
+if(Length(s)>max) then
+begin
+max:=Length(s);
+n:=i;
+s1:=s;
+end;
+end;
+Writeln('самая длинная строка'+s1);
+Writeln('ее номер'+n);
+close(fp);
+end;
+begin
+sozd(fp);
+chten(fp);
+end.
---
+++ several/shans.pas
@@ -0,0 +1,30 @@
+﻿
+var
+
+i, s, M: integer;
+
+begin
+
+ writeln('Введите число, не превыщающие 1000');  
+
+ read(M);
+
+ s:=0;
+
+  for i:=100 to M do
+
+   if ((i div 100)mod 2 <>0) and ((i div 10 mod 10)mod 2 <>0) and ((i mod 10)mod 2 <>0) then
+
+    begin
+
+     write(i:4);
+
+     s:=s+1;
+
+    end;
+
+ writeln;
+
+ writeln('Чисел:',s);
+
+end.
